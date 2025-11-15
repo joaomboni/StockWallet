2d29fa6d7fd83d4e2cc4a94439a932a3aabcb181
João Bonifácio <joaoboni017@gmail.com>
2025-11-15 20:57:39 -0300

    feat: Adicionado modulo do swagger

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const app = express()
 const routes = require("./src/routes/routes");
 const connectDB = require("./src/models/connect")
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connectDB.connect();
 app.use("/api", routes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 const port = process.env.PROXY_PORT;
 
